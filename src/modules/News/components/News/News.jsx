01e41f2749d7322bbc51e11d751b092156af100b
AnnOvs19 <annaovsyanikowa@yandex.ru>
2024-02-01 22:28:46 +0300

    Layout dropdown and create big slider

--- src/modules/News/components/News/News.jsx
+++ src/modules/News/components/News/News.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 
 import Filters from '../Filters/Filters';
 import NewsList from '../NewsList/NewsList';
-import "./news.scss"
 import ModalNotice from '../../../../components/ModalNotice/ModalNotice';
+import { newsArray } from '../../store/newsArray';
+import BigSliderList from '../BigSliderList/BigSliderList';
 
+import "./news.scss"
 
 
 const News = (props) => {
+
+    const arrBigSlider = newsArray.filter((item, index) => item.id % 2 == 0 && index < 10)
+
     return (
         <div>
+            <BigSliderList newsData={arrBigSlider} />
             <Filters />
             <NewsList />
         </div>
