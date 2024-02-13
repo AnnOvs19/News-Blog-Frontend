c8c602b0db04d491ea4dd63559be47a6bb5abaa6
AnnOvs19 <annaovsyanikowa@yandex.ru>
2024-02-13 15:11:51 +0300

    Working in registration

--- src/components/ModalLogin/ModalLogin.jsx
+++ src/components/ModalLogin/ModalLogin.jsx
@@ -1,42 +1,41 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import "./modalLogin.scss"
-import BaseInput from '../../ui/BaseInput/BaseInput';
-import BaseButton from '../../ui/BaseButton/BaseButton';
-import Login from './Login/Login';
-import Register from './Register/Register';
+import "./modalLogin.scss";
+import BaseInput from "../../ui/BaseInput/BaseInput";
+import BaseButton from "../../ui/BaseButton/BaseButton";
+import Login from "./Login/Login";
+import Register from "./Register/Register";
 
 const ModalLogin = ({ active, setActive }) => {
+  const [activeTab, setActiveTab] = useState(0);
 
-    const [activeTab, setActiveTab] = useState(0);
-
-
-    return (
-        //Следующая запись для скрытия модалки по нажатию на фон
-        <div className={active ? "modalLogin activeModal" : "modalLogin"} onClick={() => setActive(false)}>
-            <div className='modalLogin-content' onClick={(e) => e.stopPropagation()}>
-                <div className='head'>
-                    <div className='head__login' onClick={() => setActiveTab(0)}>
-                        <h3 className={activeTab == 0 ? 'base-title activeTab' : 'base-title'}>Sign in</h3>
-                    </div>
-                    <div className='head__register' onClick={() => setActiveTab(1)}>
-                        <h3 className={activeTab == 1 ? 'base-title activeTab' : 'base-title'}>New account</h3>
-                    </div>
-                </div>
-                <form className='body'>
-                    {
-                        activeTab == 0 ? (
-                            <Login />
-                        ) : (
-                            <Register />
-                        )
-                    }
-
-
-                </form>
-            </div >
-        </div >
-    );
+  return (
+    //Следующая запись для скрытия модалки по нажатию на фон
+    <div
+      className={active ? "modalLogin activeModal" : "modalLogin"}
+      onClick={() => setActive(false)}
+    >
+      <div className="modalLogin-content" onClick={(e) => e.stopPropagation()}>
+        <div className="head">
+          <div className="head__login" onClick={() => setActiveTab(0)}>
+            <h3
+              className={activeTab == 0 ? "base-title activeTab" : "base-title"}
+            >
+              Sign in
+            </h3>
+          </div>
+          <div className="head__register" onClick={() => setActiveTab(1)}>
+            <h3
+              className={activeTab == 1 ? "base-title activeTab" : "base-title"}
+            >
+              New account
+            </h3>
+          </div>
+        </div>
+        <div className="body">{activeTab == 0 ? <Login /> : <Register />}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ModalLogin;
--- src/components/ModalLogin/Register/Register.jsx
+++ src/components/ModalLogin/Register/Register.jsx
@@ -1,102 +1,158 @@
-import React, { useEffect, useState } from 'react';
-import BaseInput from '../../../ui/BaseInput/BaseInput';
-import BaseButton from '../../../ui/BaseButton/BaseButton';
+import React, { useEffect, useState } from "react";
+import BaseInput from "../../../ui/BaseInput/BaseInput";
+import BaseButton from "../../../ui/BaseButton/BaseButton";
 
 const Register = (props) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [nameDirty, setNameDirty] = useState(false);
+  const [emailDirty, setEmailDirty] = useState(false);
+  const [passwordDirty, setPasswordDirty] = useState(false);
 
-    const [nameDirty, setNameDirty] = useState(false);
-    const [emailDirty, setEmailDirty] = useState(false);
-    const [passwordDirty, setPasswordDirty] = useState(false);
+  const [nameError, setNameError] = useState(
+    "Строка с именем пользователя не может быть пустой"
+  );
+  const [emailError, setEmailError] = useState(
+    "Строка с почтой не может быть пустой"
+  );
+  const [passwordError, setPasswordError] = useState(
+    "Строка с поролем не может быть пустой"
+  );
 
-    const [nameError, setNameError] = useState("Строка с именем пользователя не может быть пустой");
-    const [emailError, setEmailError] = useState("Строка с почтой не может быть пустой");
-    const [passwordError, setPasswordError] = useState("Строка с поролем не может быть пустой");
+  const [disable, setDisable] = useState(true);
 
-    const [disable, setDisable] = useState(true);
+  const [formValid, setFormValid] = useState(false);
 
-    const [formValid, setFormValid] = useState(false);
+  useEffect(() => {
+    if (nameError || emailError || passwordError) {
+      setFormValid(false);
+      setDisable(true);
+    } else {
+      setFormValid(true);
+      setDisable(false);
+    }
+  }, [nameError, emailError, passwordError]);
 
-    useEffect(() => {
-        if (nameError || emailError || passwordError) {
-            setFormValid(false)
-            setDisable(true)
-        } else {
-            setFormValid(true)
-            setDisable(false)
+  function nameHandler(e) {
+    setName(e.target.value);
+    if (e.target.value.length < 2) {
+      setNameError("Имя должно быть длиннее 2 символов");
+      if (!e.target.value) {
+        setNameError("Строка с именем не может быть пустой");
+      }
+    } else {
+      setNameError("");
+    }
+  }
+
+  function emailHandler(e) {
+    setEmail(e.target.value);
+    const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!re.test(String(e.target.value).toLowerCase())) {
+      setEmailError("Указан неверный адрес почты");
+    } else {
+      setEmailError("");
+    }
+  }
+
+  function passwordHandler(e) {
+    setPassword(e.target.value);
+    if (e.target.value.length < 5 || e.target.value > 20) {
+      setPasswordError("Пароль должен быть длиннее 5, и не более 20 символов");
+      if (!e.target.value) {
+        setPasswordError("Строка с поролем не может быть пустой");
+      }
+    } else {
+      setPasswordError("");
+    }
+  }
+
+  function blurHandler(e) {
+    switch (e.target.name) {
+      case "name":
+        setNameDirty(true);
+        break;
+
+      case "email":
+        setEmailDirty(true);
+        break;
+
+      case "password":
+        setPasswordDirty(true);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  function userCreate() {
+    const URL = "http://localhost:6868/api/user/registration";
+
+    if (formValid) {
+      fetch(URL, {
+        method: "POST",
+        body: {
+          name: name,
+          email: email,
+          password: password
         }
-    }, [nameError, emailError, passwordError]);
+      });
+    }
+  }
 
-    function nameHandler(e) {
-        setName(e.target.value)
-        if (e.target.value.length < 2) {
-            setNameError("Имя должно быть длиннее 2 символов")
-            if (!e.target.value) {
-                setNameError("Строка с именем не может быть пустой")
-            }
-        } else {
-            setNameError("")
-        }
-    };
+  return (
+    <form className="body__register">
+      <BaseInput
+        onChange={(e) => nameHandler(e)}
+        onBlur={(e) => blurHandler(e)}
+        value={name}
+        invalid={nameDirty && nameError}
+        name={"name"}
+        type={"text"}
+        placeholder={"Name"}
+      />
+      <span className={nameError ? "error-span" : ""}>
+        {nameDirty && nameError && nameError}
+      </span>
 
-    function emailHandler(e) {
-        setEmail(e.target.value);
-        const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-        if (!re.test(String(e.target.value).toLowerCase())) {
-            setEmailError("Указан неверный адрес почты")
-        } else {
-            setEmailError("")
-        }
-    };
+      <BaseInput
+        onChange={(e) => emailHandler(e)}
+        onBlur={(e) => blurHandler(e)}
+        value={email}
+        invalid={emailDirty && emailError}
+        name={"email"}
+        type={"email"}
+        placeholder={"E-mail"}
+      />
+      <span className={emailError ? "error-span" : ""}>
+        {emailDirty && emailError && emailError}
+      </span>
 
-    function passwordHandler(e) {
-        setPassword(e.target.value);
-        if (e.target.value.length < 5 || e.target.value > 20) {
-            setPasswordError("Пароль должен быть длиннее 5, и не более 20 символов")
-            if (!e.target.value) {
-                setPasswordError("Строка с поролем не может быть пустой")
-            }
-        } else {
-            setPasswordError("")
-        }
-    };
+      <BaseInput
+        onChange={(e) => passwordHandler(e)}
+        onBlur={(e) => blurHandler(e)}
+        value={password}
+        invalid={passwordDirty && passwordError}
+        name={"password"}
+        type={"password"}
+        placeholder={"Password"}
+      />
+      <span className={passwordError ? "error-span" : ""}>
+        {passwordDirty && passwordError && passwordError}
+      </span>
 
-    function blurHandler(e) {
-        switch (e.target.name) {
-            case "name":
-                setNameDirty(true);
-                break;
-
-            case "email":
-                setEmailDirty(true);
-                break;
-
-            case "password":
-                setPasswordDirty(true);
-                break;
-
-            default:
-                break;
-        }
-    };
-
-    return (
-        <div className='body__register'>
-            <BaseInput onChange={e => nameHandler(e)} onBlur={e => blurHandler(e)} value={name} invalid={nameDirty && nameError} name={"name"} type={"text"} placeholder={"Name"} />
-            <span className={nameError ? "error-span" : ""}>{(nameDirty && nameError) && (nameError)}</span>
-
-            <BaseInput onChange={e => emailHandler(e)} onBlur={e => blurHandler(e)} value={email} invalid={emailDirty && emailError} name={"email"} type={"email"} placeholder={"E-mail"} />
-            <span className={emailError ? "error-span" : ""}>{(emailDirty && emailError) && (emailError)}</span>
-
-            <BaseInput onChange={e => passwordHandler(e)} onBlur={e => blurHandler(e)} value={password} invalid={passwordDirty && passwordError} name={"password"} type={"password"} placeholder={"Password"} />
-            <span className={passwordError ? "error-span" : ""}>{(passwordDirty && passwordError) && (passwordError)}</span>
-
-            <BaseButton disabled={!formValid && disable} styles={disable ? "modal-btn disable" : "modal-btn"}>Register</BaseButton>
-        </div>
-    );
+      <BaseButton
+        onClick={userCreate}
+        disabled={!formValid && disable}
+        styles={disable ? "modal-btn disable" : "modal-btn"}
+      >
+        Register
+      </BaseButton>
+    </form>
+  );
 };
 
 export default Register;
