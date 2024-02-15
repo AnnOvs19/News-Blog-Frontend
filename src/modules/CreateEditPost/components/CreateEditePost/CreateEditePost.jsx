c6bda026dbb0fdaacccd7fe0438ddbc45eab85bc
AnnOvs19 <annaovsyanikowa@yandex.ru>
2024-02-15 14:11:58 +0300

    Working createPost

--- src/modules/CreateEditPost/components/CreateEditePost/CreateEditePost.jsx
+++ src/modules/CreateEditPost/components/CreateEditePost/CreateEditePost.jsx
@@ -1,98 +1,139 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import "./createEditePost.scss"
-import Dropdown from '../../../../components/Dropdown/Dropdown';
-import BaseTextarea from '../../../../ui/BaseTextarea/BaseTextarea';
-import FileInput from '../../../../ui/FileInput/FileInput';
-import BaseButton from '../../../../ui/BaseButton/BaseButton';
-import BaseInput from '../../../../ui/BaseInput/BaseInput';
+import "./createEditePost.scss";
+import Dropdown from "../../../../components/Dropdown/Dropdown";
+import BaseTextarea from "../../../../ui/BaseTextarea/BaseTextarea";
+import FileInput from "../../../../ui/FileInput/FileInput";
+import BaseButton from "../../../../ui/BaseButton/BaseButton";
+import BaseInput from "../../../../ui/BaseInput/BaseInput";
 import BackgroundNews from "../../../../assets/images/backgroundNews.jpg";
-import { user } from '../../../Profile/store/mockUser';
-import { Link } from 'react-router-dom';
+import { user } from "../../../Profile/store/mockUser";
+import { Link } from "react-router-dom";
 
 const CreateEditePost = (props) => {
-    const [bacgroundNews, setBacgroundNews] = useState(BackgroundNews);
+  const [bacgroundNews, setBacgroundNews] = useState(BackgroundNews);
 
-    const [title, setTitle] = useState("");
-    const [titleDirty, setTitleDirty] = useState(false);
-    const [titleError, setTitleError] = useState("Строка с заголовком статьи не может быть пустой");
-    const [disable, setDisable] = useState(true);
+  const [title, setTitle] = useState("");
+  const [titleDirty, setTitleDirty] = useState(false);
+  const [titleError, setTitleError] = useState(
+    "Строка с заголовком статьи не может быть пустой"
+  );
+  const [disable, setDisable] = useState(true);
 
-    const [formValid, setFormValid] = useState(false);
+  const [formValid, setFormValid] = useState(false);
 
-    function handleBacgroundNews({ target: { files } }) {
-        const loadedImage = files[0];
-        setBacgroundNews(URL.createObjectURL(loadedImage))
-    };
+  function handleBacgroundNews({ target: { files } }) {
+    const loadedImage = files[0];
+    setBacgroundNews(URL.createObjectURL(loadedImage));
+  }
 
-    function titleHandler(e) {
-        setTitle(e.target.value)
-        if (e.target.value.length < 2) {
-            setTitleError("Заголовок не должен быть меньше 2х символов")
-            if (!e.target.value) {
-                setTitleError("Строка с заголовком статьи не может быть пустой")
-            }
-        } else {
-            setTitleError("")
-        }
-    };
+  function titleHandler(e) {
+    setTitle(e.target.value);
+    if (e.target.value.length < 2) {
+      setTitleError("Заголовок не должен быть меньше 2х символов");
+      if (!e.target.value) {
+        setTitleError("Строка с заголовком статьи не может быть пустой");
+      }
+    } else {
+      setTitleError("");
+    }
+  }
 
+  function blurHandler(e) {
+    switch (e.target.name) {
+      case "title":
+        setTitleDirty(true);
+        break;
 
-    function blurHandler(e) {
-        switch (e.target.name) {
-            case "title":
-                setTitleDirty(true);
-                break;
+      default:
+        break;
+    }
+  }
 
-            default:
-                break;
-        }
-    };
+  useEffect(() => {
+    if (titleError) {
+      setFormValid(false);
+      setDisable(true);
+    } else {
+      setFormValid(true);
+      setDisable(false);
+    }
+  }, [titleError]);
 
-    useEffect(() => {
-        if (titleError) {
-            setFormValid(false)
-            setDisable(true)
-        } else {
-            setFormValid(true)
-            setDisable(false)
-        }
-    }, [titleError]);
+  async function createPost(e) {
+    e.preventDefault();
 
-    return (
-        <div className='createArticle'>
-            <div className='createArticle-box'>
-                <h2 className='base-title'>Create post</h2>
-                <div className='createArticle-box__content'>
-                    <div className='createArticle-box__content-info'>
-                        <Dropdown type={1} text={"Select tag"} />
-                        <img className='img-backgroundNews' src={bacgroundNews} alt="#" />
-                        <BaseButton>
-                            <FileInput accept="image/*" onChange={handleBacgroundNews}>Edit photo</FileInput>
-                        </BaseButton>
-                        <div className='info'>
-                            <p>Title of article</p>
-                            <BaseInput onChange={(e) => titleHandler(e)} onBlur={(e) => blurHandler(e)} value={title} invalid={titleDirty && titleError} name={"title"} type={"text"} placeholder={"Your title"} styles={"postInput"} />
-                            <span className={titleError ? "error-span" : ""}>{(titleDirty && titleError) && (titleError)}</span>
-                        </div>
-                        <div className='info'>
-                            <p>Your text</p>
-                            <BaseTextarea name={"your-text"} placeholder={"Your text"} />
-                        </div>
+    const URL = "http://localhost:6868/api/post";
+    const formData = new FormData(e.target);
 
-                        <BaseButton styles={"cancelBtn"} >
-                            <Link to={"/profile"}>
-                                Cancel
-                            </Link>
-                        </BaseButton>
+    if (formValid) {
+      await fetch(URL, {
+        method: "POST",
+        body: formData
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((res) => {});
+    }
+  }
 
-                        <BaseButton disabled={!formValid && disable} styles={disable ? "disableForm" : ""}>Create post</BaseButton>
-                    </div>
-
-                </div>
+  return (
+    <div className="createArticle">
+      <div className="createArticle-box">
+        <h2 className="base-title">Create post</h2>
+        <div className="createArticle-box__content">
+          <form
+            onSubmit={(e) => {
+              createPost(e);
+            }}
+            className="createArticle-box__content-info"
+          >
+            <Dropdown type={1} text={"Select tag"} />
+            <img className="img-backgroundNews" src={bacgroundNews} alt="#" />
+            <BaseButton>
+              <FileInput accept="image/*" onChange={handleBacgroundNews}>
+                Edit photo
+              </FileInput>
+            </BaseButton>
+            <div className="info">
+              <p>Title of article</p>
+              <BaseInput
+                onChange={(e) => titleHandler(e)}
+                onBlur={(e) => blurHandler(e)}
+                value={title}
+                invalid={titleDirty && titleError}
+                name={"title"}
+                type={"text"}
+                placeholder={"Your title"}
+                styles={"postInput"}
+              />
+              <span className={titleError ? "error-span" : ""}>
+                {titleDirty && titleError && titleError}
+              </span>
             </div>
+            <div className="info">
+              <p>Your text</p>
+              <BaseTextarea name={"content"} placeholder={"Your text"} />
+            </div>
+
+            <BaseButton styles={"cancelBtn"}>
+              <Link to={"/profile"}>Cancel</Link>
+            </BaseButton>
+
+            <BaseButton
+              type={"submit"}
+              disabled={!formValid && disable}
+              styles={disable ? "disableForm" : ""}
+            >
+              Create post
+            </BaseButton>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CreateEditePost;
