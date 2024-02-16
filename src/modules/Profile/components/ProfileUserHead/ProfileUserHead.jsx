53a04beb604eb08d920164b1a76226cf5073efd2
AnnOvs19 <annaovsyanikowa@yandex.ru>
2024-02-16 21:57:16 +0300

    Render user data

--- src/modules/Profile/components/ProfileUserHead/ProfileUserHead.jsx
+++ src/modules/Profile/components/ProfileUserHead/ProfileUserHead.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import headProfileEdite from "../../../../assets/icons/headProfileEdite.svg";
 import "./profileUserHead.scss";
+import MockUser from "../../../../assets/images/mockUser.png";
 import { user } from "../../store/mockUser";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getAuth, getUserData } from "../../store/userSlice";
 
 const ProfileUserHead = () => {
+  //useSelector помогает получить значения из store
+  const isAuth = useSelector(getAuth);
+  const user = useSelector(getUserData);
+
+  const nav = useNavigate();
+
+  useEffect(() => {
+    if (!isAuth) {
+      nav("/");
+    }
+  }, []);
+
   return (
     <div className="profileUserHead">
       <div className="profileUserHead-box">
-        <img className="img-avatar" src={user.avatar} alt="#" />
+        <img className="img-avatar" src={MockUser} alt="#" />
         <div className="profileUserHead-box__content">
           <div>
             <h2 className="base-title">{user.name}</h2>
@@ -19,7 +34,10 @@ const ProfileUserHead = () => {
           </div>
 
           <p className="base-subtitle">{user.email}</p>
-          <span className="base-text">{user.status}</span>
+          <span className="base-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor.
+          </span>
         </div>
       </div>
       <div className="profileUserHead-colorLine">
