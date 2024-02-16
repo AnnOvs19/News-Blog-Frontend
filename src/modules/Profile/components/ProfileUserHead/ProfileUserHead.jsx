1ab1ae33b472c15d6f6f324c601370e860352bf2
MaxOvs19 <gshk3669@gmail.com>
2024-02-16 21:23:31 +0300

    header login

--- src/modules/Profile/components/ProfileUserHead/ProfileUserHead.jsx
+++ src/modules/Profile/components/ProfileUserHead/ProfileUserHead.jsx
@@ -1,37 +1,33 @@
-import React from 'react';
+import React from "react";
 
-
-import headProfileEdite from "../../../../assets/icons/headProfileEdite.svg"
+import headProfileEdite from "../../../../assets/icons/headProfileEdite.svg";
 import "./profileUserHead.scss";
-import { user } from '../../store/mockUser';
-import { Link } from 'react-router-dom';
+import { user } from "../../store/mockUser";
+import { Link } from "react-router-dom";
 
-const ProfileUserHead = (props) => {
+const ProfileUserHead = () => {
+  return (
+    <div className="profileUserHead">
+      <div className="profileUserHead-box">
+        <img className="img-avatar" src={user.avatar} alt="#" />
+        <div className="profileUserHead-box__content">
+          <div>
+            <h2 className="base-title">{user.name}</h2>
+            <Link to={"/profile/edit"}>
+              <img className="img-editProfile" src={headProfileEdite} alt="#" />
+            </Link>
+          </div>
 
-    return (
-        <div className='profileUserHead'>
-            <div className='profileUserHead-box' >
-                <img className='img-avatar' src={user.avatar} alt="#" />
-                <div className='profileUserHead-box__content' >
-                    <div>
-                        <h2 className='base-title'>{user.name}</h2>
-                        <Link to={"/profile/edit"}>
-                            <img className='img-editProfile' src={headProfileEdite} alt="#" />
-                        </Link>
-
-                    </div>
-
-                    <p className='base-subtitle'>{user.email}</p>
-                    <span className='base-text' >{user.status}</span>
-                </div>
-
-            </div>
-            <div className='profileUserHead-colorLine' >
-                <div className='profileUserHead-colorLine__violet' ></div>
-                <div className='profileUserHead-colorLine__yellow' ></div>
-            </div>
-        </div >
-    );
+          <p className="base-subtitle">{user.email}</p>
+          <span className="base-text">{user.status}</span>
+        </div>
+      </div>
+      <div className="profileUserHead-colorLine">
+        <div className="profileUserHead-colorLine__violet"></div>
+        <div className="profileUserHead-colorLine__yellow"></div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileUserHead;
