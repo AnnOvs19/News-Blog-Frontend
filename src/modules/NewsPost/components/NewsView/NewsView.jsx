33d12ceeae2d2b0286e2ee631e6a7b2b4a0a280d
MaxOvs19 <gshk3669@gmail.com>
2024-02-26 16:05:03 +0300

    Added likes

--- src/modules/NewsPost/components/NewsView/NewsView.jsx
+++ src/modules/NewsPost/components/NewsView/NewsView.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+
 import mockImg from "../../../../assets/images/backgroundNews.jpg";
+import like from "../../../../assets/icons/Like.svg";
 
 const NewsView = ({ data }) => {
   const pathImage = `http://localhost:6868/${data.img}`;
@@ -13,8 +15,14 @@ const NewsView = ({ data }) => {
         <span className="base-text">
           | {new Date(data.createdAt).toLocaleDateString()}
         </span>
+        <div className="newsPost-box__like">
+          <img src={like} alt="#" />
+          <p>{data.likes.length}</p>
+        </div>
       </div>
+
       <h3 className="base-title">{data.title}</h3>
+
       <p className="base-text">{data.content}...</p>
     </div>
   );
